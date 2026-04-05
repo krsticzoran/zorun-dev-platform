55695aeb64d2a19c643a6f01fbc9ab8d3a34987e
krsticzoran <zorankrstic81@gmail.com>
2026-04-05 18:22:52 +0200

    feat: add square aspect ratio and max-width constraints to ImageComponent

--- components/mdx/image-component.tsx
+++ components/mdx/image-component.tsx
@@ -3,17 +3,28 @@ import Image from "next/image";
 interface ImageComponentProps {
   src: string;
   alt: string;
+  isSquare?: boolean;
 }
 
-export function ImageComponent({ src, alt }: ImageComponentProps) {
+export function ImageComponent({
+  src,
+  alt,
+  isSquare = false,
+}: ImageComponentProps) {
   return (
-    <div className="relative w-full my-8 aspect-3/2">
+    <div
+      className={`relative w-full my-8 overflow-hidden  ${
+        isSquare
+          ? "aspect-square max-w-[650px]"
+          : "aspect-3/2 max-w-[1300px] mx-auto"
+      }`}
+    >
       <Image
         src={src}
         alt={alt}
         fill
         className="object-cover"
-        sizes="(min-width: 1024px) 100vw, 100vw"
+        sizes="(max-width: 1300px) 100vw, 1300px"
       />
     </div>
   );
