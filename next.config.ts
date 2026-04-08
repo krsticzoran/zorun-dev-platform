b4423647a82faca4042418387d5a0ce354f2530f
krsticzoran <zorankrstic81@gmail.com>
2026-04-08 21:24:51 +0200

    chore(next): only enable webp image format

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  images: {
+    qualities: [60, 75],
+    formats: ["image/webp"],
+  },
 };
 
 export default nextConfig;
